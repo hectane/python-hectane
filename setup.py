e6c1ccdad3e134dca4f7fc3990cbaaa513dac2a0
Nathan Osman <nathan@quickmediasolutions.com>
2015-09-06 15:29:37 -0600

    Bumped version to 0.2.0.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycannon',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         'requests',
@@ -15,7 +15,13 @@ setup(
     license='MIT',
     url='https://github.com/nathan-osman/pycannon',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
