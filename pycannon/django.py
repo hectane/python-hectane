f626af916ee87df303e0887ef7f853d0063889ef
Nathan Osman <nathan@quickmediasolutions.com>
2015-09-29 20:52:37 -0700

    Added very basic ability to send attachments and multipart MIME messages from Django email backend.

--- pycannon/django.py
+++ pycannon/django.py
@@ -1,5 +1,10 @@
+from __future__ import absolute_import
+
+from email.mime.base import MIMEBase
+
 from django.conf import settings
 from django.core.mail.backends.base import BaseEmailBackend
+from django.core.mail.message import EmailMultiAlternatives
 
 from .connection import Connection
 
@@ -22,20 +27,51 @@ class GoCannonBackend(BaseEmailBackend):
             password=getattr(settings, 'GO_CANNON_PASSWORD', None),
         )
 
+    def _process_attachments(self, email):
+        """
+        Convert the attachments in the email to the appropriate format for
+        sending with Connection.send().
+        """
+        for a in email.attachments:
+            if isinstance(a, MIMEBase):
+                if a.is_multipart():
+                    # TODO
+                    raise Exception("not yet implemented")
+                else:
+                    # TODO: we could avoid decoding base64 content if there
+                    # were an easy way to determine if it was already
+                    # base64-encoded
+                    yield {
+                        'content_type': a.get_content_type(),
+                        'content': a.get_payload(decode=True),
+                    }
+            else:
+                yield {
+                    'filename': a[0],
+                    'content_type': a[2],
+                    'content': a[1],
+                }
+
     def send_messages(self, emails):
         """
         Attempt to send the specified emails.
         """
         num_sent = 0
         for e in emails:
+            html = None
+            if isinstance(e, EmailMultiAlternatives):
+                for a in e.alternatives:
+                    if a[1] == 'text/html':
+                        html = a[0]
             r = self._connection.send(
                 from_=e.from_email,
                 to=e.to,
                 subject=e.subject,
                 text=e.body,
-                html=None,
+                html=html,
                 cc=e.cc,
                 bcc=e.bcc,
+                attachments=list(self._process_attachments(e)),
             )
             if 'error' not in r:
                 num_sent += 1
